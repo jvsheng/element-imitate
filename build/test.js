af2ef0fe9ed494e349af28ecbbb0cf3f043bae81
1130521172 <1130521172@qq.com>
2017-10-27 18:19:50 +0800

    build new.js

--- build/test.js
+++ build/test.js
@@ -5,6 +5,4 @@ var path = require('path');
 var fileSave = require('file-save');
 
 const componentsFile = require(path.join(__dirname, '../components.json'));
-console.log(componentsFile['test'])
-
-// fileSave("\\components.json").write('hello world', 'utf-8').end('\n');
+console.log(componentsFile['test']);
