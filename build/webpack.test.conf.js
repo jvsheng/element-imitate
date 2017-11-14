438fbd9b82a14e92899bdb55ba687a971874269f
1130521172 <1130521172@qq.com>
2017-11-14 17:56:09 +0800

    coveralls

--- build/webpack.test.conf.js
+++ build/webpack.test.conf.js
@@ -1,5 +1,6 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
+
 module.exports={
     module:{
         loaders:[{
