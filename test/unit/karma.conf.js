954cc1cfabf93bebfd116c0ec3961d5331b51500
1130521172 <1130521172@qq.com>
2017-11-15 14:10:51 +0800

    update coverage

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -1,14 +1,13 @@
 // Karma configuration
 // Generated on Tue Nov 14 2017 14:01:46 GMT+0800 (中国标准时间)
 const webpackConfig = require('../../build/webpack.test.conf');
-const webpack = require('webpack')
 
 module.exports = function(config) {
     config.set({
 
         browsers: ['PhantomJS'],
 
-        frameworks: ['mocha', 'sinon-chai'],
+        frameworks: ['mocha', 'chai'],
 
         reporters: ['spec', 'coverage'],
 
