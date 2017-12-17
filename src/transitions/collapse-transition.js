d1f587318cff3692113b14e8bcf1302cb537f971
jvsheng <huangjvsheng@hotmail.com>
2017-12-17 22:51:05 +0800

    modify collapse-transition

--- src/transitions/collapse-transition.js
+++ src/transitions/collapse-transition.js
@@ -2,43 +2,59 @@ import {addClass, removeClass} from 'main/utils/dom';
 
 class Transition {
   beforeEnter(el) {
+    console.log(el.scrollHeight);
     if (!el.dataset) el.dataset = {};
     el.dataset.overflow = el.style.overflow;
+    el.dataset.paddingTop = el.style.paddingTop;
+    el.dataset.paddingBottom = el.style.paddingBottom;
 
     addClass(el, 'collapse-transition');
     el.style.height = '0';
+    el.style.paddingTop = '0';
+    el.style.paddingBottom = '0';
   }
 
   enter(el) {
+    console.log(el.scrollHeight);
     el.style.height = el.scrollHeight + 'px';
+    el.style.paddingTop = el.dataset.paddingTop;
+    el.style.paddingBottom = el.dataset.paddingBottom;
     el.style.overflow = 'hidden';
   }
 
   afterEnter(el) {
+    console.log(el.scrollHeight);
     removeClass(el, 'collapse-transition');
     el.style.height = '';
     el.style.overflow = el.dataset.overflow;
   }
 
   beforeLeave(el) {
+    console.log(el.scrollHeight);
     if (!el.dataset) el.dataset = {};
     el.dataset.overflow = el.style.overflow;
+    el.dataset.paddingTop = el.style.paddingTop;
+    el.dataset.paddingBottom = el.style.paddingBottom;
 
-    addClass(el, 'collapse-transition');
+    el.style.overflow = 'hidden';
     el.style.height = el.scrollHeight + 'px';
   }
 
   leave(el) {
+    console.log(el.scrollHeight);
+    addClass(el, 'collapse-transition');
     setTimeout(function() {
       el.style.height = '0';
-      el.style.overflow = 'hidden';
-    }, 20);
+    }, 40);
   }
 
   afterLeave(el) {
+    console.log(el.scrollHeight);
     removeClass(el, 'collapse-transition');
     el.style.height = '';
     el.style.overflow = el.dataset.overflow;
+    el.style.paddingTop = el.dataset.paddingTop;
+    el.style.paddingBottom = el.dataset.paddingBottom;
   }
 }
 
