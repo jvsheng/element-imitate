4b97aaa089ff0374bbb91a56ccbc2bc86bd275b1
1130521172 <1130521172@qq.com>
2017-12-19 09:51:53 +0800

    modify collapse

--- packages/collapse/src/collapse-item.vue
+++ packages/collapse/src/collapse-item.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="el-collapse-item" :class="{'is-active': isActive}">
-    <div class="el-collapse-item_header" @click="handleClick">
-      {{title}}
-      <slot name="title"></slot>
-    </div>
-    <el-collapse-transition>
-      <div class="el-collapse-item_wrap"
-           v-show="isActive">
-        <div class="el-collapse-item_content">
-          <slot></slot>
+    <div class="el-collapse-item" :class="{'is-active': isActive}" role="tab">
+        <div class="el-collapse-item_header"
+             @click="handleClick"
+             role="button"
+             @keyup.space.enter.stop="handleEnterClick"
+             tabindex="0"
+             :class="{'focusing': focusing}"
+             @focus="handleFocus"
+             @blur="focusing = false">
+            {{title}}
+            <slot name="title"></slot>
         </div>
-      </div>
-    </el-collapse-transition>
-  </div>
+        <el-collapse-transition>
+            <div class="el-collapse-item_wrap"
+                 v-show="isActive">
+                <div class="el-collapse-item_content">
+                    <slot></slot>
+                </div>
+            </div>
+        </el-collapse-transition>
+    </div>
 </template>
 
 <script>
@@ -23,6 +30,13 @@
     name: 'ElCollapseItem',
 
     mixins: [Emitter],
+
+    data() {
+      return {
+        focusing: false,
+        isClick: false
+      };
+    },
 
     components: {
       ElCollapseTransition
@@ -45,7 +59,21 @@
     },
 
     methods: {
+      handleFocus() {
+        setTimeout(() => {
+          if (!this.isClick) {
+            this.focusing = true;
+          } else {
+            this.isClick = false;
+          }
+        }, 50);
+      },
       handleClick() {
+        this.dispatch('ElCollapse', 'item-click', this.name);
+        this.focusing = false;
+        this.isClick = true;
+      },
+      handleEnterClick() {
         this.dispatch('ElCollapse', 'item-click', this.name);
       }
     }
